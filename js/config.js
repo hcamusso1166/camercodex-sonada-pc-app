176875fad0d5cc6678ce417720b50dd18bece968
hcamusso1166 <164682455+hcamusso1166@users.noreply.github.com>
2026-08-24 14:47:54 -0300

    Integrate SHOW_SKETCH into BTI V2 image encore

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // config.js
-const appVersion = "1.01 DEVNarnia";  // Aquí puedes cambiar la versión cuando sea necesario.
+const appVersion = "1.10 DEVCroquis";  // Aquí puedes cambiar la versión cuando sea necesario.
